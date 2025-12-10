7779077eb6ccb98facfaac7dd8c28b7287b81324
suhail sama <suhailsama89@gmail.com>
2025-12-10 20:16:31 +0530

    first

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-yellow-300 text-4xl font-extrabold">
+        TAILWIND COLOR TEST
+
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+
